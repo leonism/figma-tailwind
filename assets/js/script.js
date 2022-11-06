7968ff2180e5f171a62ba55b5abc0440878d66f7
gerry leo nugroho <gerryleonugroho@gmail.com>
2022-11-06 11:07:57 +0700

    i finally got them working this time

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,14 @@ const toggleNavigation = () => {
   // checking the class name if navigation link has "-left-[250px]" class or not
   if (navigationLinks.classList.contains("-left-[250px]")) {
 
-    // if found, remove them
-    // show
+    // if found, show them
     navigationLinks.classList.replace("-left-[250px]", "left-0")
     overlay.classList.remove(["hidden"])
 
     // else
   } else {
     
-    // if not, add them
-    // hide
+    // if not, hide them
     navigationLinks.classList.replace("left-0", "-left-[250px]")
     overlay.classList.add(["hidden"])
   }
