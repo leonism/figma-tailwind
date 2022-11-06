f26e8a96f6efce1bce39bbce81cdf73c9caf492f
gerry leo nugroho <gerryleonugroho@gmail.com>
2022-11-06 10:52:48 +0700

    adding close button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 const toggleNavigation = () => {
   const navigationLinks = document.querySelector("[data-nav-links]")
+  const overlay = document.querySelector(["data-nav-link-overlay"])
 
-  // checking the class name if navigation link has hidden class or no
-  if (navigationLinks.classList.contains("hidden")) {
+  // checking the class name if navigation link has "-left-[250px]" class or not
+  if (navigationLinks.classList.contains("-left-[250px]")) {
 
     // if found, remove them
-    navigationLinks.classList.remove("hidden")
+    // show
+    navigationLinks.classList.replace("-left-[250px]", "left-0")
+    overlay.classList.remove(["hidden"])
 
     // else
   } else {
     
     // if not, add them
-    navigationLinks.classList.add("hidden")
+    // hide
+    navigationLinks.classList.replace("left-0", "-left-[250px]")
+    overlay.classList.add(["hidden"])
   }
-
 }
 
 // console.log(navigationLinks)
